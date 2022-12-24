bd8222acb7ccac54b1af4c4dd6551f977defb8b2
priyanshu-creator <priyanshu.creator.dev@gmail.com>
2022-12-24 16:28:59 +0530

    server update

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e)=>{
 
     // fetch data from server
 
-    const response = await fetch(`http://localhost:5000/`, {
+    const response = await fetch(`https://chat-soms.onrender.com/`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
