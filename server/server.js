f149ec53c21e297dcd1cabe0e12f3cdb16a52265
Priyanshu Verma <112266318+priyanshu-creator@users.noreply.github.com>
2022-12-31 16:37:24 +0530

    Model changes

--- server/server.js
+++ server/server.js
@@ -49,11 +49,11 @@ app.post('/' , async (req, res) => {
             const response = await openai.createCompletion({
                 model: "text-davinci-003",
                 prompt: `${prompt}`,
-            temperature: 0,
+            temperature: 0.9,
             max_tokens: 3000,
             top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0,
+            frequency_penalty: 0,
+            presence_penalty: 0.6,
             });
 
             res.status(200).send({
